1d142188bc7043bf96deda74606c95adf35187e0
Dominique <dlsykes91@gmail.com>
2020-06-07 13:43:30 -0400

    Fetch Satellite Data

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,7 +1,11 @@
 import { ThrowStmt } from '@angular/compiler';
 
 export class Satellite {
-
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
 
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name
